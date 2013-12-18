0c0aa71a04213e1403a006a981625c89b621603f
Stuart Kurtz <stuart@cs.uchicago.edu>
2013-12-18 11:30:19 -0600

    0.1.2.0
    (1) Added output functions showCSV, writeCSV, and the utility procedure
    encodeCSVField.
    (2) Changed the behavior of parseCSV and readCSV to filter out trivial
    records. Added the functions parseRawCSV and readRawCSV which don’t
    filter out trivial records.

--- Text/CSV.hs
+++ Text/CSV.hs
@@ -6,17 +6,23 @@
   Maintainer  :  stuart@cs.uchicago.edu
   Stability   :  experimental
  
-  A library for parsing comma separated values (CSV).
+  A module for handling comma-separated values (CSV), either in Strings or files.
 -}
 
 module Text.CSV (
 	parseCSV,
 	readCSV,
-	prune
+	parseRawCSV,
+	readRawCSV,
+	prune,
+	encodeCSVField,
+	showCSV,
+	writeCSV
 ) where
 
 import Control.Monad
 import Data.Char
+import Data.List (intercalate)
 import Text.ParserCombinators.ReadP
 
 {-
@@ -36,19 +42,42 @@ parse p s = case [a | (a,"") <- readP_to_S p s] of
 {- |
   The 'parseCSV' function parses a 'String' containing comma separated
   values (CSV) as a list of records, each of which is a list of fields
-  of type 'String'. Note that CSV is ambiguous, in that a newline may be
-  intended either as a record terminator, or as a record separator. The
-  'parseCSV' function treats newlines as record separators, which allows
+  of type 'String'. This function, unlike 'parseRawCSV', filters out 
+  trivial records, i.e., records that consist of a single, white-space only
+  field.
+-} 
+
+parseCSV :: String -> [[String]]
+parseCSV = filter (not . trivial) . parseRawCSV where
+    trivial :: [String] -> Bool
+    trivial [x] = all isSpace x
+    trivial _ = False
+
+{- |
+  The 'readCSV' function parses a file containing comma separated values
+  (CSV) as a list of records, each of which is a list of fields
+  of type 'String'. This function uses 'parseCSV' for parsing.
+-}
+
+readCSV :: FilePath -> IO [[String]]
+readCSV = fmap parseCSV . readFile
+
+{- |
+  The 'parseRawCSV' function parses a 'String' containing comma separated
+  values (CSV) as a list of records, each of which is a list of fields
+  of type 'String'. Note that CSV specification is ambiguous, in that a terminal
+  newline may be intended either as a record terminator, or as a record separator. The
+  'parseRawCSV' function treats newlines as record separators, which allows
   it to successfully parse in either case, albeit with a spurious final
-  record consisting of a single, empty field, on a 'String' in which
+  record consisting of a single, empty field, given an argument 'String' in which
   newline was intended as a record terminator.
   
   Client code should be aware of the possibility (even likelihood) of
-  encountering records that consist of a single, empty field.
+  encountering trivial records.
 -} 
   
-parseCSV :: String -> [[String]]	
-parseCSV  = parse csv where
+parseRawCSV :: String -> [[String]]	
+parseRawCSV  = parse csv where
 	csv = record `sepBy1` newline
 	record = field `sepBy1` char ','
 	field = complete simpleField <++ complete quotedField
@@ -63,14 +92,14 @@ parseCSV  = parse csv where
 	newline = string "\r\n" <++ string "\n" <++ string "\r"
 
 {- |
-  The 'readCSV' function parses a file containing comma separated values
+  The 'readRawCSV' function parses a file containing comma separated values
   (CSV) as a list of records, each of which is a list of fields
-  of type 'String'. The same caveats regarding CSV that are noted in
-  documentation for 'parseCSV' apply to this function as well.
+  of type 'String'. This function uses 'parseRawCSV' for parsing, and so
+  may contain trivial records.
 -}
 
-readCSV :: FilePath -> IO [[String]]
-readCSV = fmap parseCSV . readFile
+readRawCSV :: FilePath -> IO [[String]]
+readRawCSV = fmap parseRawCSV . readFile
 
 {- |
   The 'prune' function is used to filter the values obtained by the application
@@ -82,3 +111,41 @@ prune :: [[String]] -> [[String]]
 prune = filter nontrivial where
 	nontrivial [x] = any (not . isSpace) x
 	nontrivial _ = True
+
+{- |
+  The 'encodeCSVField' function will quote a 'String' that contains carriage
+  returns, linefeeds, double-quotes, or commas, properly quoting for CSV any
+  double-quoted fields.
+-}
+
+encodeCSVField :: String -> String
+encodeCSVField s
+    | any (`elem` ['\r','\n','"',',']) s = "\"" ++ csvQuote s ++ "\""
+    | otherwise = s
+    where
+        csvQuote = foldr quoteChar ""
+        quoteChar '"' s = "\"\"" ++ s
+        quoteChar c s = c:s
+
+{- |
+  The 'showCSV' function converts a [['String']] to a 'String', following the
+  conventions of CSV, i.e., the result string will be valid CSV. The intent is
+  that
+
+>       readCSV . showCSV = id
+
+  This may not be precisely so if the original CSV contains trivial records, as
+  they will be elided by the process.
+
+-}
+
+showCSV :: [[String]] -> String
+showCSV = unlines . map (intercalate "," . map encodeCSVField)
+
+{- |
+  The 'writeCSV' function takes a [['String']], converting it to what is intended to
+  be equivalent CSV (note the caveat in 'showCSV'), and writing the results to the
+  indicated file.
+-}
+
+writeCSV fname = writeFile fname . showCSV
